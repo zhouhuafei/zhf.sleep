a3c7e23259554297a6539d96b15451cce372ce2f
zhouhuafei <1123486116@qq.com>
2022-03-26 12:44:05 +0800

    style: 💄 ts2js

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,17 +6,17 @@ const src = './src'
 const dist = './dist'
 const isProduction = process.env.NODE_ENV === 'production'
 
-function ts2cjs () {
+function ts2js () {
   return gulp.src(`${src}/**/*.ts`)
     .pipe(babel(babelConfig))
     .pipe(gulp.dest(dist))
 }
 
 function watcher () {
-  gulp.watch(`${src}/**/*.ts`, gulp.series(ts2cjs))
+  gulp.watch(`${src}/**/*.ts`, gulp.series(ts2js))
 }
 
-const series: Array<any> = [ts2cjs]
+const series: Array<any> = [ts2js]
 if (!isProduction) {
   series.push(watcher)
 }
