c7c4e98c3361777546436fa6fc6922c12f122f52
zhouhuafei <1123486116@qq.com>
2022-05-09 12:59:39 +0800

    perf: ⚡️ 使用gulp-changed解决gulp每次全量编译文件的问题

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,6 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
+import changed from 'gulp-changed'
 import babelConfig from './babel.config'
 
 const src = './src'
@@ -8,6 +9,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 function ts2js () {
   return gulp.src(`${src}/**/*.ts`)
+    .pipe(changed(dist, { extension: '.js' }))
     .pipe(babel(babelConfig))
     .pipe(gulp.dest(dist))
 }
